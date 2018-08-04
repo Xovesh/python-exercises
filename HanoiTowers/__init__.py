83316228da42d7aa9659a67a68ea296c44a250d3
Xovesh <39794796+Xovesh@users.noreply.github.com>
2018-08-04 14:53:02 +0200

    04/08/2018

--- HanoiTowers/__init__.py
+++ HanoiTowers/__init__.py
@@ -2,6 +2,6 @@ import Hanoi
 import time
 
 start_time = time.time()
-Hanoi.Hanoi(int(input("Introduce a number: ")))
+Hanoi.Hanoi(int(input("Put a number: ")))
 print("Time used")
 print("--- %s seconds ---" % (time.time() - start_time))
