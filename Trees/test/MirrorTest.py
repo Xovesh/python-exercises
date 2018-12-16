1c9887cd9a3f86e34baf073d2a71508d4fbef0f5
Xovesh <39794796+Xovesh@users.noreply.github.com>
2018-12-16 21:27:49 +0100

    12/16/2018

--- Trees/test/MirrorTest.py
+++ Trees/test/MirrorTest.py
@@ -12,5 +12,6 @@ magictree.right.left = TreeNode.TreeNode("F")
 magictree.right.right = TreeNode.TreeNode("G")
 
 print(TreeSearch.TreeSearch.breadthfirstsearch(magictree))
-print(TreeSearch.TreeSearch.breadthfirstsearch(Mirror.mirror(magictree)))
-print(Mirror.ismirror(magictree, Mirror.mirror(magictree)))
+a = Mirror.mirror(magictree)
+print(TreeSearch.TreeSearch.breadthfirstsearch(a))
+print(Mirror.ismirror(magictree, a))
